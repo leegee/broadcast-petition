6e6634719bce16cf26e5e0bc8366f5f8f80feb0c
Lee Goddard <leegee@gmail.com>
2025-09-27 02:21:17 +0200

    Closed state

--- src/components/PetitionLink.tsx
+++ src/components/PetitionLink.tsx
@@ -5,21 +5,35 @@ import { Show } from "solid-js";
 
 export default function PetitionLink() {
     return (
-        <Show when={!petitionMeta.closed_at}>
-            <article class={styles.sign + " border margin padding"}>
-                <h2 class="padding">
-                    <Flag />
-                    <span>
-                        Sign at{" "}
-                        <a
-                            class="left-margin small-margin"
-                            href={`https://petition.parliament.uk/petitions/${PETITION_ID}/signatures/new`}
-                        >
-                            {`petition.parliament.uk/petitions/${PETITION_ID}`}
-                        </a>
-                    </span>
-                </h2>
-            </article>
-        </Show>
+        <>
+            <Show when={!petitionMeta.closed_at}>
+                <div class={styles.sign + " border margin padding"}>
+                    <h2 class="padding">
+                        <Flag />
+                        <span>
+                            Sign at{" "}
+                            <a
+                                class="left-margin small-margin"
+                                href={`https://petition.parliament.uk/petitions/${PETITION_ID}/signatures/new`}
+                            >
+                                {`petition.parliament.uk/petitions/${PETITION_ID}`}
+                            </a>
+                        </span>
+                    </h2>
+                </div>
+            </Show>
+
+            <Show when={!!petitionMeta.closed_at}>
+                <div class={styles.sign + " border margin padding"} >
+                    <h2 class="padding">
+                        <Flag />
+                        <span>This petition closed at
+                            {new Date(petitionMeta.closed_at!).toLocaleDateString()}
+                            {new Date(petitionMeta.closed_at!).toLocaleTimeString()}
+                        </span>
+                    </h2>
+                </div>
+            </Show>
+        </>
     );
 }
